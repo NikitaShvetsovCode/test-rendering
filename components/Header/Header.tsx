0564750737c91d9f09d6021ed1f17d5c98dcad94
NikitaShvetsovCode <nshvtsv@gmail.com>
2025-09-28 17:16:57 +0500

    лого и стили

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,11 +1,22 @@
 import Burger from "@/components/Burger/Burger";
 import styles from "./index.module.scss";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <div className={`container ${styles.innerHeader}`}>
-        <>123</>
+        <Link href="/">
+          <Image
+            src="/kube-logo.png"
+            alt="Поиск работы без стресса: как автоотклики экономят время и нервы"
+            width={47}
+            height={45}
+            className={styles.imageLogo}
+            quality={100}
+          />
+        </Link>
         <Burger></Burger>
       </div>
     </header>
